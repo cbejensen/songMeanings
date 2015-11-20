c4dcdc452df679853e253c43956a619b505e7c09
Christian <cbejensen@gmail.com>
2015-11-19 19:45:09 -0700

    working ui-links

--- js/MainController.js
+++ js/MainController.js
@@ -1,19 +1,33 @@
 angular.module('app')
 .controller('MainController', function($scope, service, Spotify) {
-  $scope.test = 'test';
+  
+  $scope.test = 'whatwhat';
+  
+  $scope.login = function() {
+    Spotify.login().then(function() {
+      $scope.getPlaylist();
+    }) 
+  }
   
   $scope.getPlaylist = function() {
     service.getPlaylist().then(function(data) {
-      console.log(data);
       $scope.playlist = data.tracks.items;
+      console.log($scope.playlist);
     });
   }
+  
+  /*remove ' from song title*/
+  $scope.linkify= function(name) {
+    var arr = name.split('');
+    arr.forEach(function(elem, index) {
+      if(elem === '\'') {
+        arr.splice(index, 1);
+      }
+    })
+    return arr.join('');
+  }
+  
   $scope.getPlaylist();
   
-  
-  //    Spotify.login().then(function(res) {
-//      console.log(res)
-//      
-//    });
   
 })
--- js/app.js
+++ js/app.js
@@ -1,9 +1,24 @@
-angular.module('app', ['spotify'])
-.config(function (SpotifyProvider) {
+angular.module('app', ['spotify', 'ui.router'])
+.config(function (SpotifyProvider, $stateProvider, $urlRouterProvider) {
   SpotifyProvider.setClientId('456095fae6884551b223950e2a72f04a');
   SpotifyProvider.setRedirectUri('http://localhost:8080/auth');
   SpotifyProvider.setScope('user-read-private playlist-read-private playlist-modify-private playlist-modify-public');
+  SpotifyProvider.setAuthToken(window.localStorage['spotify-token']);
   
-  // If you already have an auth token
-  SpotifyProvider.setAuthToken('BQAxN1A4Ve2cyHgOKFoGZqfC--0M3DHjq2DD_WHjuwjc_Z30atU1siiKlp_Y0iXKq9xhOanjTEWemsTyUvL-TaAeJrqs3wo-alfTbif3rGr9LrC2nOHOFYsFG8NfdLGg2gWzt4Uxo8Zf7fL67pHNl5NTArUrgnx4vOE5GSo3H3EwDZ_wFmYqE6SBHh7CQyun9QmZowH2HpgfB3SnWgIQTpUZgnfgHKnj_3dBo2UlhFZtmb_Ti9eiIb39_VRwLl818Mg');
+  $urlRouterProvider.otherwise("/");
+  
+  $stateProvider
+    .state('main', {
+      url: "/",
+      templateUrl: "main.html"
+    })
+    .state('sorry', {
+      url: "/sorry",
+      templateUrl: "sorry.html",
+    })
+    .state('track', {
+      url: "/track/:id",
+      template: '<h1>hey!</h1>'
+    })
+  
 });
