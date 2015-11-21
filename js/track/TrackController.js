5d8981e43106ab467a64fa2c44a6ba026028823a
Christian <cbejensen@gmail.com>
2015-11-21 12:53:48 -0700

    added track preview

--- js/track/TrackController.js
+++ js/track/TrackController.js
@@ -1,6 +1,9 @@
 angular.module('TrackSuite')
 .controller('TrackController', function($scope, $stateParams, trackService, $firebaseObject, Spotify) {
   
+  var ref = new Firebase("https://song-meanings.firebaseio.com");
+  $scope.trackData = $firebaseObject(ref);
+  
   $scope.getTrack = function(id) {
     trackService.getTrack(id).then(function(data) {
       $scope.track = data;
@@ -9,7 +12,11 @@ angular.module('TrackSuite')
   }
   $scope.getTrack($stateParams.id);
   
-  var ref = new Firebase("https://song-meanings.firebaseio.com");
-  $scope.trackData = $firebaseObject(ref);
+  /* alerts user if no preview of track is available*/
+  $scope.previewTrack = function(track) {
+    if(!$scope.track.preview_url) {
+      alert('Sorry - there\'s no preview available for this song!')
+    }
+  }
   
 })
